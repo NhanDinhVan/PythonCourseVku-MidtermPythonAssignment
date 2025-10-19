09d8be3e10b7518b6f5b37855994091dacdd09d2
nhandinhvan <vannhan2409@gmail.com>
2025-10-19 19:06:08 +0700

    chore(): done part 1 - exp 10

---
+++ src/chapter-2/part-1/exp_10.py
@@ -0,0 +1,8 @@
+length = float(input("Enter the length of the room: "))
+width = float(input("Enter the width of the room: "))
+
+area = length * width
+
+print("The length of the room is: ", length)
+print("The width of the room is: ", width)
+print(f"The area of the room is: {area:.2f} square meters")
