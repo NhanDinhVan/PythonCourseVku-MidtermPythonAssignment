1b32f3ca732a19928a1692e44ebf63494b05741f
nhandinhvan <vannhan2409@gmail.com>
2025-10-19 16:02:42 +0700

    chore(): done part 1 - exp 1

---
+++ src/chapter-2/part-1/exp_1.py
@@ -0,0 +1,10 @@
+st = input("Enter a string: ")
+
+st_lower = st.lower()
+st_title = st.title()
+st_upper = st.upper()
+
+
+print("Lower case: ", st_lower)
+print("Title case: ", st_title)
+print("Upper case: ", st_upper)
