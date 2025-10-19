aba1a7ddad32e6b4fd56a661655ea6981062a3e8
nhandinhvan <vannhan2409@gmail.com>
2025-10-19 19:05:31 +0700

    chore(): done part 1 - exp 6

---
+++ src/chapter-2/part-1/exp_6.py
@@ -0,0 +1,10 @@
+from math import pi
+r = input("Enter radius: ")
+h = input("Enter height: ")
+
+r = float(r)
+h = float(h)
+
+volume =  pi * r * r * h
+
+print("Volume of the cylinder is: ", volume)
